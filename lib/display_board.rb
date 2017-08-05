a09b19310c14d5b0ad1bad50339748c77431e63a
Pablo Almancio Gargallo <al315490@uji.es>
2017-08-05 00:36:42 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  table=Array.new()
+  table << " #{board[0]} | #{board[1]} | #{board[2]} "
+  table.push("-----------")
+  table.push(" #{board[3]} | #{board[4]} | #{board[5]} ")
+  table.push("-----------")
+  table.push(" #{board[6]} | #{board[7]} | #{board[8]} ")
+  for i in table
+    puts i
+  end
+end
